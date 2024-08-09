0c46cd4534de35308635c4d3a0c4cf15edc9b0af
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-09 06:07:55 +0530

    Create Bug1.java

---
+++ Bug1.java
@@ -0,0 +1,10 @@
+public class Bug1{
+public static void main(String[]args){
+String str1 = "hello";
+String str2 = new String("hello");
+if (str1 == str2) {
+    System.out.println("Strings are equal");
+}
+  // Find Bugs in Logic
+}
+}
