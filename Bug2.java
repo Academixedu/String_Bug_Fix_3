9beab47c3da37c25009903a07dfcd2704c2ecb11
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-08-09 15:58:27 +0530

    string _logics3

--- Bug2.java
+++ Bug2.java
@@ -1,8 +1,12 @@
 public class Bug2{
 public static void main(String[]args){
 String str = "";
-if (str.equals(null)) {
-    System.out.println("String is null");
+if (str == null) {
+    System.out.println("string is null");
+}
+else if (str.isEmpty()){
+  System.out.println("String is empty");
+  
 }
 }
   // Fix Bug In Logic
