cf3d5482a91270e1489adbabb9ede3b015b4daf6
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-09 06:18:09 +0530

    Create Bug4.java

---
+++ Bug4.java
@@ -0,0 +1,7 @@
+public class Bug4{
+public static void main(String[]args){
+String str = "Hello Hello";
+String newStr = str.replace("Hello", "Hi"); // Replaces all occurrences
+// Only Replace "Hi" With First Occurence of Hello
+}
+}
