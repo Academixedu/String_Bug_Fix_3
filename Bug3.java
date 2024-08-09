83ff8c7062506bec0149205a87a2ba468eb9979a
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-08-09 18:11:22 +0530

    changes

--- Bug3.java
+++ Bug3.java
@@ -1,12 +1,12 @@
 public class Bug3{
 public static void main(String[]args){
 String str = "Hello";
-for(int i=0;i<str.length();i++){
-
-char ch = str.charAt(i); // Index out of bounds
+// char ch = str.charAt(10); // Index out of bounds
 // Prepare a Condition to Check the Index of a String only upto it's Last Index and Should not Exceed it
-System.out.println(ch+ " " +i);
-}
-    
+    int index=10;
+    if(index>str.length()){
+        System.out.println("index out of bound");
+    }
+
 }
 }
