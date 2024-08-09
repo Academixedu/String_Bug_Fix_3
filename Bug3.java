32caecbb13e813d00a75ef20a612441f285ecb04
ganeshteki <ganeshteki2001@gmail.com>
2024-08-09 19:58:02 +0530

    string bugs

--- Bug3.java
+++ Bug3.java
@@ -1,7 +1,14 @@
-public class Bug3{
-public static void main(String[]args){
-String str = "Hello";
-char ch = str.charAt(10); // Index out of bounds
-// Prepare a Condition to Check the Index of a String only upto it's Last Index and Should not Exceed it
+public class Bug3 {
+    public static void main(String[] args) {
+        String str = "Hello";
+        int index = 10; 
+
+        if (index >= 0 && index < str.length()) {
+            char ch = str.charAt(index);
+            System.out.println("Character at index " + index + ": " + ch);
+        } else {
+            System.out.println("Index out of bounds");
+        }
+    }
 }
-}
+
