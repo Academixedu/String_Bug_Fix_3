682d12760ad63950f8d89c348a2ee2d317778aa0
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-09 16:43:30 +0530

    Swarupa

--- Bug3.java
+++ Bug3.java
@@ -1,7 +1,15 @@
 public class Bug3{
 public static void main(String[]args){
 String str = "Hello";
+int a=10;
+if(a>=0 && a<=str.length()){
 char ch = str.charAt(10); // Index out of bounds
 // Prepare a Condition to Check the Index of a String only upto it's Last Index and Should not Exceed it
+    System.out.println("ch");
+}
+else
+{
+    System.out.println("Index of out bounds");
+}
 }
 }
