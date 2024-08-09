a41dafe2af7fd6901fa20430a176f4cb1a470ec2
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-09 13:01:47 +0530

    Update Bug2.java

--- Bug2.java
+++ Bug2.java
@@ -1,9 +1,12 @@
-public class Bug2{
-public static void main(String[]args){
-String str = "";
-if (str.equals(null)) {
-    System.out.println("String is null");
-}
-}
-  // Fix Bug In Logic
+ public class Bug2 {
+    public static void main(String[] args) {
+        String str = "";
+        
+        // Check if str is null
+        if (str == null) {
+            System.out.println("String is null");
+        } else if (str.equals("")) {
+            System.out.println("String is empty");
+        }
+    }
 }
