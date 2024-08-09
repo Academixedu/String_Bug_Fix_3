90d247230c90b86d185885ec222003eeac8056ad
ratan-17 <rkmule2002@gmail.com>
2024-08-09 18:49:52 +0530

    ratan

--- Bug4.java
+++ Bug4.java
@@ -1,7 +1,11 @@
-public class Bug4{
-public static void main(String[]args){
-String str = "Hello Hello";
-String newStr = str.replace("Hello", "Hi"); // Replaces all occurrences
-// Only Replace "Hi" With First Occurence of Hello
-}
+public class Bug4
+{
+public static void main(String[]args)
+    {
+            String str = "Hello Hello";
+            String newStr = str.replaceFirst("Hello", "Hi"); // Replaces all occurrences
+            // Only Replace "Hi" With First Occurence of Hello
+         System.out.println(newStr);
+
+ }
 }
