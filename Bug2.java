c1f5a3cbd9791d1deab4632e9f1db73f66df4c5e
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-09 19:07:12 +0530

    bugs

--- Bug2.java
+++ Bug2.java
@@ -1,9 +1,13 @@
 public class Bug2{
-public static void main(String[]args){
-String str = "";
-if (str.equals(null)) {
-    System.out.println("String is null");
-}
-}
-  // Fix Bug In Logic
-}
+  public static void main(String[]args){
+  String str = "";
+  if (str.equals(null)) {
+      System.out.println("String is null");
+  if (str == null) {
+      System.out.println("string is null");
+  }}
+  else if (str.isEmpty()){
+    System.out.println("String is empty");
+  
+  }
+    }}
