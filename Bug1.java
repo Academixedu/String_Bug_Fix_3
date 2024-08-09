db9231d394099fc92a4329742f0d3548b03022c2
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-08-09 17:45:32 +0530

    sarvesh

--- Bug1.java
+++ Bug1.java
@@ -2,8 +2,10 @@ public class Bug1{
 public static void main(String[]args){
 String str1 = "hello";
 String str2 = new String("hello");
-if (str1 == str2) {
+if (str1.equals(str2)) {
     System.out.println("Strings are equal");
+}else{
+  System.out.println("Strings are not equal");
 }
   // Find Bugs in Logic
 }
