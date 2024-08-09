7cd27eaea778f791f32dd3237f3084bc21c6bfdd
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-09 06:12:50 +0530

    Create Bug3.java

---
+++ Bug3.java
@@ -0,0 +1,7 @@
+public class Bug3{
+public static void main(String[]args){
+String str = "Hello";
+char ch = str.charAt(10); // Index out of bounds
+// Prepare a Condition to Check the Index of a String only upto it's Last Index and Should not Exceed it
+}
+}
