ff3afc583e06aae8872250855e07cfa32b1faeb5
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-08-09 17:29:16 +0530

    From Karthik

--- Bug2.java
+++ Bug2.java
@@ -1,9 +1,8 @@
 public class Bug2{
 public static void main(String[]args){
 String str = "";
-if (str.equals(null)) {
+if (str.equals("")) {
     System.out.println("String is null");
 }
 }
-  // Fix Bug In Logic
 }
