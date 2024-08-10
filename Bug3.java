6e93186c7fc452ba7c0baac1774c40e37b9fb683
Verat-hub <venkey482thalka@gmail.com>
2024-08-10 20:44:19 +0530

    Venkat

--- Bug3.java
+++ Bug3.java
@@ -1,7 +1,10 @@
 public class Bug3{
 public static void main(String[]args){
 String str = "Hello";
-char ch = str.charAt(10); // Index out of bounds
+for(int i=0;i<str.length();i++){
+char ch = str.charAt(i); // Index out of bounds
 // Prepare a Condition to Check the Index of a String only upto it's Last Index and Should not Exceed it
+System.out.println(ch);
+}
 }
 }
